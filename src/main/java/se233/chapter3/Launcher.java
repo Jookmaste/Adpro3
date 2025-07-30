0c2dc84fdd41f49cfa8031608546982a344196a1
ไอ่ปุณณ์ <Punnlaowatchara@gmail.com>
2025-07-30 23:21:31 +0700

    Almost done

--- src/main/java/se233/chapter3/Launcher.java
+++ src/main/java/se233/chapter3/Launcher.java
@@ -1,7 +1,26 @@
 package se233.chapter3;
 
-public class Launcher{
-    public static void main (String[]arg){
+import javafx.application.Application;
+import javafx.application.HostServices;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Launcher extends Application {
+    public static Stage primaryStage;
+    public static HostServices hs;
+    @Override
+    public void start(Stage stage) throws Exception {
+        primaryStage = stage;
+        hs = getHostServices();
+        FXMLLoader fxmlLoader = new FXMLLoader(Launcher. class.getResource("main-view. fxml"));
+                Scene scene = new Scene(fxmlLoader.load());
+//        stage.setTitle("Indexer");
+//        stage.setScene(scene);
+//        stage.show();
+        primaryStage.setTitle("Indexer");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+    static void main(String[] args) { launch(args); }
 }
